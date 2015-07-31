4acbfdf6a3dbe0951426d0148278c37c8cb7e15e
Joel Hammond-Turner <joel@Joels-Mac-Pro.local>
2015-07-31 20:10:14 +0100

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hammondTurnerorgukApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hammondTurnerorgukApp
+ */
+angular.module('hammondTurnerorgukApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hammondTurnerorgukApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hammondTurnerorgukApp
+ */
+angular.module('hammondTurnerorgukApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
